62545185bad423f1c751c7d2008155f635ad59d4
Mikita Shchyhelski <mikita.shchyhelski@sivintech.com>
2019-05-30 00:01:18 +0300

    init

---
+++ web/buildtools/webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rootDir = path.resolve(__dirname, '..', '../');
+
+const babelLoaderConfiguration = {
+  test: /\.js$/,
+  include: [
+    path.resolve(rootDir, 'index.js'),
+    path.resolve(rootDir, 'src'),
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/env', '@babel/react'],
+      plugins: ['react-native-web'],
+    },
+  },
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  },
+};
+
+module.exports = {
+  entry: [path.resolve(rootDir, 'index.js')],
+  output: {
+    filename: 'bundle.web.js',
+    path: path.resolve(rootDir, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [babelLoaderConfiguration, imageLoaderConfiguration],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './web/public/index.html',
+    }),
+  ],
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+    extensions: ['.web.js', '.js'],
+  },
+};
